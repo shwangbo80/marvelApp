bc7a69b8df88198484c8290030258c5c2c0e43e5
shwangbo80 <shwangbo80@gmail.com>
2021-12-03 14:32:29 -0800

    fixed responsive search bar

--- src/MainComponent.js
+++ src/MainComponent.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from "react"
+import {React, useState} from "react"
 import {Button, Form, Container, Row, Col, Image} from "react-bootstrap"
 
 export default function MainComponent() {
@@ -7,9 +7,9 @@ export default function MainComponent() {
   const hash = "abb8397e8f309853a6979c66696c8ee7"
   const [character, setCharacter] = useState("")
   const [isLoading, setLoading] = useState(false)
-  const apiUrl = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${character}&ts=${timestamp}&apikey=${apiKey}&hash=${hash}`
-
   const [apiData, setApiData] = useState([])
+  const [errorStatus, setError] = useState(false)
+  const apiUrl = `https://gateway.marvel.com/v1/public/characters?nameStartsWith=${character}&ts=${timestamp}&apikey=${apiKey}&hash=${hash}`
 
   function GenerateApi() {
     fetch(apiUrl)
@@ -31,45 +31,45 @@ export default function MainComponent() {
         <Container className="mt-5">
           {apiData.data.results.map((item, key) => {
             return (
-              <div key={key}>
+              <div key={item.id}>
                 <Row className="pt-5">
-                  <Col md={3}>
+                  <Col md={4}>
                     <Image
                       className="mb-5"
-                      alt="jean"
+                      alt={item.name}
                       src={`${item.thumbnail.path}.${item.thumbnail.extension}`}
                       fluid
                     />
                   </Col>
                   <Col md={6}>
-                    <h4>{item.name}</h4>
-                    <p>{item.description}</p>
+                    <h2>{item.name}</h2>
+                    <h5>{item.description}</h5>
                   </Col>
                 </Row>
                 <Row className="mt-4 pb-3">
                   <Col md={4}>
                     <h4>Comics</h4>
                     {item.comics.items.map((comics, key) => {
-                      return <p key={key}>{comics.name}</p>
+                      return <p>{comics.name}</p>
                     })}
                   </Col>
                   <Col md={4}>
                     <h4>Series</h4>
                     {item.series.items.map((comics, key) => {
-                      return <p key={key}>{comics.name}</p>
+                      return <p>{comics.name}</p>
                     })}
                   </Col>
                   <Col md={4}>
                     <h4>Stories</h4>
                     {item.stories.items.map((comics, key) => {
-                      return <p key={key}>{comics.name}</p>
+                      return <p>{comics.name}</p>
                     })}
                   </Col>
                   <Col md={12} className="mt-3">
                     {item.urls.map((urls, key) => {
                       return (
                         <div className="d-inline-block mx-2">
-                          <p key={key} className="">
+                          <p className="">
                             <a href={urls.url} target="_blank">
                               {urls.type}
                             </a>
@@ -104,21 +104,24 @@ export default function MainComponent() {
           <Col md={2}></Col>
           <Col md={8}>
             <Row className="px-5 mb-5">
+              <Col sm={1}></Col>
               <Col sm={10}>
                 <Form.Control
                   className="mt-3"
                   size="md"
-                  type="text"
-                  placeholder="Search for Heroes"
+                  type="search"
+                  placeholder="Search
+                  for Heroes"
                   value={character}
-                  onChange={onChange}
-                />
-              </Col>
-              <Col sm={2}>
-                <Button onClick={GenerateApi} className="btn-danger mt-3">
+                  onChange={onChange}></Form.Control>
+                <Button
+                  onClick={GenerateApi}
+                  type="submit"
+                  className="btn-danger mt-3">
                   Search
                 </Button>
               </Col>
+              <Col sm={1}></Col>
             </Row>
           </Col>
           <Col md={2}></Col>
